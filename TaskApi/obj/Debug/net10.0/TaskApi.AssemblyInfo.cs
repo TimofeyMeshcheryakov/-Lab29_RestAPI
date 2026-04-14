befa58e7cd385b6c19264e0261a17e988f6f23b7
duxa4854 <nzaichenko531@gmail.com>
2026-04-14 13:46:05 +0300

    Step 2: TaskItem model and DTOs created

--- TaskApi/obj/Debug/net10.0/TaskApi.AssemblyInfo.cs
+++ TaskApi/obj/Debug/net10.0/TaskApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4e789fd777e1fb02e8cfeb8ddff2f1265d38d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
